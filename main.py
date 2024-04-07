84d1d4d01a2eb931f1cbf0654c76055791638213
Ahmed Abdul Faheem <100959005+ahmed20455@users.noreply.github.com>
2024-04-07 12:01:50 +0530

    Day2- Band Name Generator

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city_name = input("What's the name of the city you grew up in ?")
+pet_name = input("What's your pet name ?")
+print(f'Your band name can be {city_name} {pet_name}')

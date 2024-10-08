ad953a716483a937a444b93b52865c65986ed008
Ahmed Abdul Faheem <100959005+ahmed20455@users.noreply.github.com>
2024-10-08 14:37:23 +0530

    Update main.py

--- Day-23/main.py
+++ Day-23/main.py
@@ -32,4 +32,5 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.reset_game()
 screen.exitonclick()

9166127b6736a255edb3e24737a45caa12bfbeb2
Ahmed Abdul Faheem <100959005+ahmed20455@users.noreply.github.com>
2024-10-08 14:30:29 +0530

    Create scoreboard.py

---
+++ Day-23/scoreboard.py
@@ -0,0 +1,26 @@
+FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-220, 250)
+        self.write("Level=" + str(self.level), align="center", font=FONT)
+
+    def level_increment(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def reset_game(self):
+        self.write("You Lost", align="center", font=FONT)
+        self.level = 1
+        self.update_scoreboard()

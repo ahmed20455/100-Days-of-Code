4b2a9970584d3008b96ddf65b2ae72c191c6aaf0
Ahmed Abdul Faheem <100959005+ahmed20455@users.noreply.github.com>
2024-10-08 14:29:08 +0530

    Create main.py

---
+++ Day-23/main.py
@@ -0,0 +1,35 @@
+import time
+from turtle import Screen
+from player import Player
+from scoreboard import Scoreboard
+from car_manager import CarManager
+
+screen = Screen()
+screen.bgcolor("white")
+screen.title("Crossing Roads")
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+scoreboard = Scoreboard()
+car_manager = CarManager()
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+    if player.ycor() >= 280:
+        scoreboard.level_increment()
+        player.reset_pos()
+        car_manager.car_speed_increment()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+screen.exitonclick()

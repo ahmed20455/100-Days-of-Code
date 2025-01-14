df0ccf445a2aada9f8d07e974b60991e84ab7a15
Ahmed Abdul Faheem <100959005+ahmed20455@users.noreply.github.com>
2025-01-14 20:44:45 +0530

    Update main.py

--- Day-26/main.py
+++ Day-26/main.py
@@ -6,6 +6,12 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for(index, row) in data.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-h_word = str(input("Enter a word: ")).upper()
-ans = [nato_dict[letter]  for letter in h_word]
-print(ans)
+def nato():
+  h_word = str(input("Enter a word: ")).upper()
+  try:
+    ans = [nato_dict[letter]  for letter in h_word]
+  except KeyError:
+    print("Sorry, only letters are in the alphabet")
+  else:
+    print(ans)
+nato()
